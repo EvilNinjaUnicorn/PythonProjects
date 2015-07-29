87f9c9931e15bb6a49729b6aa659c7a7a7d6b0c2
root <root@emanuel-desktop>
2015-07-29 15:06:14 +0200

    Aktualisiert

---
+++ Objectiv/Test/Object.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 5, 2015
+
+@author: emanuel
+'''
+
+class Object(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        print "test"
+        
---
+++ Objectiv/Test/Test.py
@@ -0,0 +1,15 @@
+'''
+Created on Jul 5, 2015
+
+@author: emanuel
+'''
+from Object import Object
+
+
+def main():
+    object = Object()
+
+
+
+if __name__ == '__main__':
+    main()
